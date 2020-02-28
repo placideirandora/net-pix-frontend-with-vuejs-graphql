54f7a354dc0f7a72c2c0a115ac05bfc8aedb2a65
Placide IRANDORA <placideirandora@gmail.com>
2020-02-28 13:39:46 +0200

    Fix the issue: 'DeprecationWarning: collection.ensureIndex is deprecated. Use createIndexes instead'

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ dotenv.config({ path: 'variables.env' });
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true
   })
   .then(() => {
     console.log('Database Connected');
