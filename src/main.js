863916630dde0f49dd8bd1a49e03101d6e3d0c18
placiderapson <placideirandora@gmail.com>
2021-02-26 08:54:42 +0200

    Use Vue Toastification for Notifications

--- src/main.js
+++ src/main.js
@@ -4,10 +4,29 @@ import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
 import '@babel/polyfill';
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
+
 import { createProvider } from './graphql/apolloClient';
 import Notification from '../src/components/Shared/Notification';
 import DataNotFound from '../src/components/Shared/DataNotFound';
 import ContentLoader from '../src/components/Shared/ContentLoader';
+
+const options = {
+  position: 'top-right',
+  timeout: 5000,
+  closeOnClick: true,
+  pauseOnFocusLoss: true,
+  pauseOnHover: true,
+  draggable: true,
+  draggablePercent: 0.6,
+  showCloseButtonOnHover: false,
+  closeButton: 'button',
+  icon: true,
+  rtl: false
+};
+
+Vue.use(Toast, options);
 
 Vue.config.productionTip = false;
 Vue.component('Notification', Notification);
